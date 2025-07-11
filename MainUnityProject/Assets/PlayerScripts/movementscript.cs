24e85ae02d804c15fc17450a3d85dafcde7959c9
Programmering <teknisk+programmering-GDC2025-GR1@game.unf.dk>
2025-07-11 15:49:48 +0200

    added jump feature

--- MainUnityProject/Assets/PlayerScripts/movementscript.cs
+++ MainUnityProject/Assets/PlayerScripts/movementscript.cs
@@ -1,14 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class movementscript : MonoBehaviour
 {
 
-    public float speed = 6;
+    public float speed = 6, jumpforce = 270, jumpdelay = 0.1f;
+    bool jumping;
+    Rigidbody rb;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -19,5 +22,24 @@ public class movementscript : MonoBehaviour
         
         
         transform.position += movement * speed;
+        
+        //jump
+        if (Input.GetKey(KeyCode.Space) && !jumping)
+        {
+            if (Physics.Raycast(transform.position - new Vector3(0, 1, 0), Vector3.down, .1f))
+            {
+                rb.AddForce(Vector3.up * jumpforce);
+                StartCoroutine(jumpDelay());
+            }
+        }
     }
+
+    IEnumerator jumpDelay()
+    {
+        jumping = true;
+        yield return new WaitForSeconds(jumpdelay);
+        jumping = false;
+        StopAllCoroutines();
+    }
+    
 }
