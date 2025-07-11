223d9dfb55eb7cf263f602042815ee17941a7e5d
Programmering <teknisk+programmering-GDC2025-GR1@game.unf.dk>
2025-07-11 15:40:04 +0200

    InstantiateObject Script complete

--- MainUnityProject/Assets/PlayerScripts/InstantiateObjects.cs
+++ MainUnityProject/Assets/PlayerScripts/InstantiateObjects.cs
@@ -6,8 +6,9 @@ public class InstantiateObjects : MonoBehaviour
     public GameObject obj;
     GameObject instObj;
 
-    [SerializeField] float objVelF = 0;
-    [SerializeField] float objVelS = 0;
+    [SerializeField] float objVelX = 0;
+    [SerializeField] float objVelZ = 0;
+    [SerializeField] float objVelY = 0;
     
     void Update()
     {
@@ -15,8 +16,8 @@ public class InstantiateObjects : MonoBehaviour
         {
             instObj = Instantiate(obj, transform.position + transform.forward * 1, new Quaternion());
             instObj.AddComponent<Rigidbody>();
-            instObj.GetComponent<Rigidbody>().linearVelocity = objVelF * transform.forward;
-            instObj.GetComponent<Rigidbody>().linearVelocity = objVelS * transform.right;
+            instObj.GetComponent<Rigidbody>().linearVelocity = objVelX * transform.forward + objVelZ * transform.right + objVelY * transform.up;
+            
         }
     }
 }
