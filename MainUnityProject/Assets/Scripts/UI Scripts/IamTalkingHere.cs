860450acd417f6093454c52c9e4b032253934152
Programmering <teknisk+programmering-GDC2025-GR1@game.unf.dk>
2025-07-13 22:30:07 +0200

    dialog

--- MainUnityProject/Assets/Scripts/UI Scripts/IamTalkingHere.cs
+++ MainUnityProject/Assets/Scripts/UI Scripts/IamTalkingHere.cs
@@ -8,11 +8,15 @@ public class IamTalkingHere : MonoBehaviour, IInteractable
 {
 
     [SerializeField] string[] dialog1;
-    [SerializeField] bool iamGuard;
     [SerializeField] GameObject dialogUI;
     [SerializeField]GameObject btnUI, obj;
-    public bool talking;
     [SerializeField]float delay, countdown;
+
+    
+    public bool talking, Bdialog1;
+    TextMeshProUGUI dialogText;
+    int currentDialog;
+    
     
     public void Interact(int input, GameObject Obj)
     {
@@ -21,42 +25,51 @@ public class IamTalkingHere : MonoBehaviour, IInteractable
         obj.transform.parent.GetComponent<movementscript>().enabled = false;
         obj.transform.parent.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         obj.transform.parent.GetComponent<CameraMovementScript>().enabled = false;
+        obj.GetComponent<IInteractor>().enabled = false;
 
 
+        talking = true;
+        btnUI.SetActive(false);
+        dialogUI.SetActive(true);
+        dialogText = dialogUI.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+
+        currentDialog = 0;
+        
         if (input == 0)
         {
-            StartCoroutine(Dialog());
+            Bdialog1 = true;
         }
         else if (input == 1)
         {
             
         }
         
-        
 
     }
 
-    IEnumerator Dialog()
+    void Update()
     {
-        talking = true;
-        btnUI.SetActive(false);
-        dialogUI.SetActive(true);
-        int currentDialog = 0;
-        TextMeshProUGUI dialogText = dialogUI.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();        
-        
-        while (talking)
+
+        if (talking)
         {
-            if (currentDialog == dialog1.Length)
+            if (Bdialog1)
             {
-                talking = !talking;
-                dialogUI.SetActive(false);
+                if (currentDialog == dialog1.Length)
+                {
+                    Bdialog1 = false;
+                    talking = false;
+                    dialogUI.SetActive(false);
+                    obj.transform.parent.GetComponent<movementscript>().enabled = true;
+                    obj.transform.parent.GetComponent<CameraMovementScript>().enabled = true;
+                    obj.GetComponent<IInteractor>().enabled = true;
+                }
+                else if(Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    print("test");
+                    currentDialog++;
+                }
+                dialogText.SetText(dialog1[currentDialog]);
             }
-            else if(Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                currentDialog++;
-            }
-            dialogText.SetText(dialog1[currentDialog]);
-            yield return new WaitForEndOfFrame();
         }
     }
     
