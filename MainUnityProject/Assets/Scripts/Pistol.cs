40757c684e453eff87c861232a486f89739fc42d
Programmering <teknisk+programmering-GDC2025-GR1@game.unf.dk>
2025-07-15 11:27:37 +0200

    more work on dialogue and UI

---
+++ MainUnityProject/Assets/Scripts/Pistol.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Pistol : MonoBehaviour, IInteractable
+{
+    public void Interact(int input, GameObject interactor)
+    {
+        
+    }
+}
--- MainUnityProject/Assets/Scripts/UI Scripts/DialogueData.cs
+++ MainUnityProject/Assets/Scripts/UI Scripts/DialogueData.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 [System.Serializable]
 public class DialogueData
 {
+    public string Speaker;
+
     public string soundName;
 
     public string text;
-
-    public string Speaker;
 }
